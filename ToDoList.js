e46e1f55c6df3de728cf98433c7a0daf4f552c5c
Dhananjai Pratap Singh <143176852+DhananjaiPS@users.noreply.github.com>
2024-11-20 23:48:41 +0530

    Add files via upload

--- ToDoList.js
+++ ToDoList.js
@@ -1,31 +1,53 @@
 var cnt=1;
 function AddTask(){
     
-    // const inputV =prompt("Enter the Task :");
+    
     const div=document.createElement("div");
     div.className='box';
-    const input=document.createElement("input");
-    input.innerText=input;
-    input.style.fontSize="2vw";
-    input.style.width="10wv";
-    const completeBtn=document.createElement("button");
-    completeBtn.innerHTML='<i class="fa-regular fa-circle-check"></i>';
-    completeBtn.addEventListener("click",()=>{
-        completeBtn.parentElement.remove();
-        cnt=cnt-1;
-    })
+    
+    // const input=document.createElement("input");
+    // input.innerText=input;
+    // input.style.fontSize="2vw";
+    // input.style.width="10wv";
+
+    const input =document.getElementById("inputtext");
+    const inputbox=document.createElement("span");
+    inputbox.innerHTML=input.value;
+    inputbox.className="s2";
     const but1=document.createElement("button");
     but1.innerHTML='<i class="fa-solid fa-circle-xmark"></i>';
     // div.append(but1);
+    but1.className="b1";
     but1.addEventListener("click",()=>{
         but1.parentElement.remove();
         cnt=cnt-1;
     })
+
+
+    const completeBtn=document.createElement("button");
+    completeBtn.innerHTML='<i class="fa-regular fa-circle-check"></i>';
+    completeBtn.className="b2";
+    completeBtn.addEventListener("click",()=>{
+        inputbox.style.textDecorationLine = "line-through";
+        completeBtn.style.color="green";
+        inputbox.style.color="red";
+        // cnt=cnt-1;
+    })
+
+
     const serialNo=document.createElement("span");
     serialNo.innerText=cnt++;
-    div.append(serialNo,input,but1,completeBtn);
+    serialNo.className="s1";
+    div.append(serialNo,inputbox,but1,completeBtn);
     const par=document.getElementById("box-main");
+    input.value="";
     par.append(div);
+   
+    
+    
+    
+    
+
 }
 
 
---
+++ todo3.js
@@ -0,0 +1,8 @@
+const obj={
+    "raju":1,
+    "somi":2,
+}
+obj["yogi"]=9
+console.log(obj)
+delete obj.raju
+console.log(obj);
