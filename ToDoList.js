29b65b97df9bb5e8eb56f472da1a88b850565dff
Dhananjai Pratap Singh <143176852+DhananjaiPS@users.noreply.github.com>
2024-11-21 00:11:41 +0530

    Add files via upload

--- ToDoList.js
+++ ToDoList.js
@@ -1,53 +1,62 @@
-var cnt=1;
-function AddTask(){
-    
-    
-    const div=document.createElement("div");
-    div.className='box';
-    
-    // const input=document.createElement("input");
-    // input.innerText=input;
-    // input.style.fontSize="2vw";
-    // input.style.width="10wv";
+let cnt = 1;
+const btn = document.getElementsByClassName("btn")[0];
+const input = document.getElementById("inputtext");
 
-    const input =document.getElementById("inputtext");
-    const inputbox=document.createElement("span");
-    inputbox.innerHTML=input.value;
-    inputbox.className="s2";
-    const but1=document.createElement("button");
-    but1.innerHTML='<i class="fa-solid fa-circle-xmark"></i>';
-    // div.append(but1);
-    but1.className="b1";
-    but1.addEventListener("click",()=>{
-        but1.parentElement.remove();
-        cnt=cnt-1;
-    })
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        btn.click();
+    }
+});
 
+function AddTask() {
+    if (input.value.trim() === "") {
+        alert("Task cannot be empty!");
+        return;
+    }
 
-    const completeBtn=document.createElement("button");
-    completeBtn.innerHTML='<i class="fa-regular fa-circle-check"></i>';
-    completeBtn.className="b2";
-    completeBtn.addEventListener("click",()=>{
+    const div = document.createElement("div");
+    div.className = "box";
+
+    const serialNo = document.createElement("span");
+    serialNo.innerText = cnt++;
+    serialNo.className = "s1";
+
+    const inputbox = document.createElement("span");
+    inputbox.innerHTML = input.value;
+    inputbox.className = "s2";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+    deleteBtn.className = "b1";
+    deleteBtn.addEventListener("click", () => {
+        div.remove();
+        updateSerialNumbers(); // Update serial numbers dynamically
+    });
+
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+    completeBtn.className = "b2";
+    completeBtn.addEventListener("click", () => {
         inputbox.style.textDecorationLine = "line-through";
         completeBtn.style.color="green";
         inputbox.style.color="red";
-        // cnt=cnt-1;
-    })
+    });
 
+    div.append(serialNo, inputbox, deleteBtn, completeBtn);
 
-    const serialNo=document.createElement("span");
-    serialNo.innerText=cnt++;
-    serialNo.className="s1";
-    div.append(serialNo,inputbox,but1,completeBtn);
-    const par=document.getElementById("box-main");
-    input.value="";
-    par.append(div);
-   
-    
-    
-    
-    
-
+    const parent = document.getElementById("box-main");
+    input.value = ""; // Clear input field
+    parent.append(div);
 }
+// function updateSerialNumbers(){
+//     const serials=document.querySelectorAll(".s1");
+//     serials.forEach((serial,index))
+// }
 
-
+function updateSerialNumbers() {
+    const serials = document.querySelectorAll(".s1");
+    serials.forEach((serial, index) => {
+        serial.innerText = index + 1;
+    });
+    cnt = serials.length + 1; // Adjust counter
+}
